0d7e07b3aa2c6115c0cd36887deba2976ac7bd4f
Nikunj <f20250867@pilani.bits-pilani.ac.in>
2026-03-20 22:56:33 +0530

    dbsqlite -> postgresql

--- DVM_project/settings.py
+++ DVM_project/settings.py
@@ -114,8 +114,12 @@ SITE_ID = 1
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DVM',
+        'USER': 'postgres',
+        'PASSWORD': 'nikunj',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
